2e8474e021b9bd5079dc3c44f2f2ad7f9d2d4e30
RonaldColyar <phantexcreations@gmail.com>
2022-04-29 00:53:22 -0600

    Setup

---
+++ src/integration/house_of_iot.rs
@@ -0,0 +1,49 @@
+//! A simple example of hooking up stdin/stdout to a WebSocket stream.
+//!
+//! This example will connect to a server specified in the argument list and
+//! then forward all data read on stdin to the server, printing out all data
+//! received on stdout.
+//!
+//! Note that this is not currently optimized for performance, especially around
+//! buffer management. Rather it's intended to show an example of working with a
+//! client.
+//!
+//! You can use this example together with the `server` example.
+
+use std::env;
+
+use futures_util::{future, pin_mut, StreamExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+
+async fn connect_and_begin_listening(connect_addr: &str) {
+    let url = url::Url::parse(connect_addr).unwrap();
+
+    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    let (write, read) = ws_stream.split();
+
+    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+    let ws_to_stdout = { read.for_each(|message| async {}) };
+
+    pin_mut!(stdin_to_ws, ws_to_stdout);
+    future::select(stdin_to_ws, ws_to_stdout).await;
+}
+
+// Our helper method which will read data from stdin and send it along the
+// sender provided.
+async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    let mut stdin = tokio::io::stdin();
+    loop {
+        let mut buf = vec![0; 1024];
+        let n = match stdin.read(&mut buf).await {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+        buf.truncate(n);
+        tx.unbounded_send(Message::binary(buf)).unwrap();
+    }
+}
+
+async fn authenticate() {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+pub mod integration {
+    pub mod house_of_iot;
+}
+pub mod communication {
+    pub mod rabbit;
+}
+
 fn main() {
     println!("Hello, world!");
 }
